c52ff46016dfac108916df31dedfac121af06dfa
B.LIKHITHA <likkilikitha214@gmail.com>
2025-10-04 09:28:58 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,10 @@
+a=10
+def area_of_square(side):
+    return side**2
+def calculator(x,y):
+    print("Addition:",x+y)
+    print("subtraction:",x-y)
+    print("multiplication:",x*y)
+    print("division:",x/y)
+
+    
